9802af26504bf9e5869e6d2418637be875a7eac1
zanjs <suihuasheng@gmail.com>
2019-06-05 17:03:17 +0800

    up gp

--- data.go
+++ data.go
@@ -1,9 +1,16 @@
 package main
 
+// Attribute is
+type Attribute struct {
+	Name string `json:"name"`
+	Sort int8   `json:"sort"`
+}
+
 //ResData is
 type ResData struct {
-	URL    string `json:"url"`
-	Title  string `json:"title"`
-	Thumb  string `json:"thumb"`
-	Domain string `json:"domain"`
+	URL       string      `json:"url"`
+	Title     string      `json:"title"`
+	Thumb     string      `json:"thumb"`
+	Domain    string      `json:"domain"`
+	Attribute []Attribute `json:"attribute"`
 }
